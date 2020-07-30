2f88bd04000ced78de4122f90578d556a85bce3b
lexiconPDX [alias] <chaungemail@gmail.com>
2020-07-29 19:28:42 -0700

    'again'

--- src/functions.js
+++ src/functions.js
@@ -26,3 +26,17 @@ function addf(x){
   return x +y
   }
 }
+ //a beautiful and simple way to throw four functions onto a line
+const liftf = fn => x => y => fn(x,y)
+
+function curry(fn, x){ //params: binary func + arg
+  return liftf(fn)(x) //return func that can take a second arg
+// now at 34 specs, 26 failures
+}
+
+// const curry = (fn, x) => liftf => fn(x,y) does not work
+function twice (fn) {
+  return function (x) {
+    return fn(x,x)
+  }
+}
